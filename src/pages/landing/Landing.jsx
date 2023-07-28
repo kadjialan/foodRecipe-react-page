bfb87d92b29abebee64209c2851b48405bb2348b
kadjialan <alankadji7@gmail.com>
2023-07-28 12:29:10 +0100

    adding content to the landing page

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -4,7 +4,15 @@ import './Landing.css';
 function landing() {
   return (
     <div className="landing-container">
-      <h1>welcome to recipe</h1>
+      <h1>welcome to your recipe</h1>
+      <p>Guide lines</p>
+      <ul>
+        <li>Press the get started button to navigate to the main page</li>
+        <li>
+          Press the Add bottom at the top right side of the page to add a meal
+        </li>
+        <li>A recipe should contain</li>
+      </ul>
     </div>
   );
 }
